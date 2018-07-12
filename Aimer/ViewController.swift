7e80f2eb22a1ec70adc9fe55653abe5cd1f9c48d
Theresa <sc@TheresadeMac-mini.local>
2018-07-12 17:11:18 +0800

    initial

---
+++ Aimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aimer
+//
+//  Created by Theresa on 2018/7/12.
+//  Copyright © 2018年 Carolar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
